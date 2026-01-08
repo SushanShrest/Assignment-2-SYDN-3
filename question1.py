c7b90e25b78224c176a70fbf0922de2052590474
Sushan Shrestha <129778454+SushanShrest@users.noreply.github.com>
2026-01-08 20:12:49 +1100

    Final Changes to question 1 and worked on File read

--- question1.py
+++ question1.py
@@ -17,15 +17,57 @@ def encrypt_rules(char):
     else:
         return char
 
+def decrypt_rules(char):
+    if 'a' <= char <= 'm':
+        return chr((ord(char) - ord('a') - (shift1 * shift2)) % 13 + ord('a'))
+
+    elif 'n' <= char <= 'z':
+        return chr((ord(char) - ord('n') + (shift1 + shift2)) % 13 + ord('n'))
+
+    elif 'A' <= char <= 'M':
+        return chr((ord(char) - ord('A') + shift1) % 13 + ord('A'))
+
+    elif 'N' <= char <= 'Z':
+        return chr((ord(char) - ord('N') - (shift2 ** 2)) % 13 + ord('N'))
+
+    else:
+        return char
+
 def encryption():
-    text = "abc m n xyz ABC M N XYZ"
+    with open("raw_text.txt", "r") as file:
+        text = file.read()
+
     encrypted_text = ""
     
     for char in text:
         encrypted_text += encrypt_rules(char)
+    
+    with open("encrypted_text.txt", "w") as file:
+        file.write(encrypted_text)
 
     print("File encrypted successfully.")
-    print(encrypted_text)
+
+def decryption():
+    with open("encrypted_text.txt", "r") as file:
+        encrypted_text = file.read()
+
+    decrypted_text = ""
+    
+    for char in encrypted_text:
+        decrypted_text += decrypt_rules(char)
+    
+    with open("decrypted_text.txt", "w") as file:
+        file.write(decrypted_text)
+
+    print("File decrypted successfully.")
+
+def verify():
+    with open("raw_text.txt", "r") as file1, open("decrypted_text.txt", "r") as file2:
+        if file1.read() == file2.read():
+            print("Verification Successful: Content matches.")
+        else:
+            print("Verification Failed: Content does not match.")
 
 encryption()
-
+decryption()
+verify()
