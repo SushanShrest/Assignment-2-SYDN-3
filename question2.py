61ad3aa3ee232b8ebdfeda5423f9fe05adfe5557
prasant <rayandhamala1999@gamil.com>
2026-01-11 07:29:57 +1100

    worked on condition 2 finding largest temperature range station.txt

--- question2.py
+++ question2.py
@@ -28,12 +28,12 @@ season_temperatures = {
 
 station_temperatures = {}
 
-for csv_file in os.listdir(TEMPERATURE_FOLDER_PATH):
-    if csv_file.endswith(".csv"):
-         path = os.path.join(TEMPERATURE_FOLDER_PATH, csv_file)
+for csv_filename in os.listdir(TEMPERATURE_FOLDER_PATH):
+    if csv_filename.endswith(".csv"):
+         path = os.path.join(TEMPERATURE_FOLDER_PATH, csv_filename)
 
-         with open(path, "r") as csv_file:
-            reader = csv.DictReader(csv_file)
+         with open(path, "r") as file:
+            reader = csv.DictReader(file)
 
             for row in reader:
                 station_name = row["STATION_NAME"]
@@ -51,9 +51,34 @@ for csv_file in os.listdir(TEMPERATURE_FOLDER_PATH):
 
                     season_temperatures[season].append(temperature)
                     station_temperatures[station_name].append(temperature)
-                    
+
 with open("average_temperature.txt", "w", encoding="utf-8") as f:
-    for season in season_temperatures:
-        temperature = season_temperatures[season]
+    for season, temperature in season_temperatures.items():
+        # temperature = season_temperatures[season]
         average = sum(temperature) / len(temperature)
         f.write(f"{season}: {average:.1f}°C\n")
+
+max_range = 0
+station_ranges = {}
+
+for station in station_temperatures:
+    temps = station_temperatures[station]
+    if not temps:
+        continue
+
+    max_temp = max(temps)
+    min_temp = min(temps)
+    temp_range = max_temp - min_temp
+
+    station_ranges[station] = (temp_range, max_temp, min_temp)
+
+    if temp_range > max_range:
+        max_range = temp_range
+
+with open("largest_temp_range_station.txt","w", encoding="utf-8") as f:
+    for station, (range_val ,range_max_temp, range_min_temp) in station_ranges.items():
+        if range_val == max_range:
+            f.write(
+                f"{station}: Range {range_val:.1f}°C (Max: {range_max_temp:.1f}°C, Min: {range_min_temp:.1f}°C)\n"
+            )
+            
